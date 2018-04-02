ff8b3e95e5a6c649e07c39dca26f4f1fc32c636b
hyyc116 <hyyc116@gmail.com>
2018-04-01 20:35:38 -0400

    add

--- temporal_abalysis.py
+++ temporal_abalysis.py
@@ -31,7 +31,6 @@ def citation_distribution(pid_cits_path,com_IDs_year_path):
     logging.info('load published years ...')
     com_IDs_year = json.loads(open(com_IDs_year_path).read())
 
-
     logging.info("Paper grouping ...")
     ## group paper into three groups
     xmin,xmax = group_papers(pid_cit_num,'data/paper_groups.jpg')
@@ -40,7 +39,7 @@ def citation_distribution(pid_cits_path,com_IDs_year_path):
     ## get highly cited papers
     highly_cited_papers = {}
     for pid in pid_citations.keys():
-        if pid>=xmax:
+        if len(pid_citations[pid])>=xmax:
             cpid_list = []
             for cpid in pid_citations[pid]:
                 cpid_list.append([cpid,com_IDs_year.get(cpid,-1)])
@@ -49,17 +48,45 @@ def citation_distribution(pid_cits_path,com_IDs_year_path):
 
     open('data/highly_cited_papers.json','w').write(json.dumps(highly_cited_papers))
 
-    logging.info('saved to data/highly_cited_papers.json.')
+    logging.info('Plotting highly cited papers ..')
+    high_pids = highly_cited_papers.keys()
+    total_num = len(high_pids)
+    rows = (total_num/5)+1
+    fig,axes = plt.subplots(rows,5,figsize=(25,rows*5))
+    highly_cited_papers_ids = []
+    for i,pid in enumerate(high_pids):
 
+        highly_cited_papers_ids.append(pid)
 
-def plot_highly_cited_papers(highly_cited_papers_path):
-    pass
+        y0 = com_IDs_year[pid]
+        ax = axes[i/5,i%5]
+        citation_list = highly_cited_papers[pid]
+        year_num =defaultdict(int)
+        for cpid,year in citation_list:
+            year_num[year]+=1
+
+        xs = []
+        ys = []
+        for year in sorted(year_num.keys()):
+            xs.append(year-y0)
+            ys.append(year_num[year])
+
+        ax.plot(xs,ys)
+        ax.set_title(pid)
+        ax.set_xlabel('years after publication')
+        ax.set_ylabel('number of citations')
+
+    open('data/highly_cited_papers_ids.txt','w').write('\n'.join(highly_cited_papers_ids))
+    plt.savefig('pdf/plots_of_highly_cited_papers.jpg',dpi=200)
+    logging.info('figure saved to pdf/plots_of_highly_cited_papers.jpg.')
+
     
 
 if __name__ == '__main__':
     pid_cits_path = 'data/pid_cits.txt'
     com_IDs_year_path = 'data/com_ids_year.json'
     citation_distribution(pid_cits_path,com_IDs_year_path)
+
 
 
 
