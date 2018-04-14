b50e0f75957b106c36181ccbc381194bf35762fc
黄永 <huangyong@140-182-74-92.ssl-vpn.indiana.edu>
2018-04-14 17:35:31 -0400

    add

--- plot_temporal_analysis.py
+++ plot_temporal_analysis.py
@@ -42,8 +42,8 @@ def plot_curve_of_all_attrs(highly_cited_paper_age_stat_path):
 		labels = ['numer of citations','late endorser','connector','normal endorser','depth','ICRs','number of subjects','subjects']
 		values = zip(*values)
 		
-		fig,axes = plt.subplots(1,3,figsize=(21,5))
-		ax = axes[0]
+		fig,axes = plt.subplots(2,2,figsize=(28,5))
+		ax1 = axes[0,1]
 		for i,label in enumerate(labels):
 
 			if label == 'depth':
@@ -61,24 +61,32 @@ def plot_curve_of_all_attrs(highly_cited_paper_age_stat_path):
 				ICRs = values[i]
 				continue
 
-			ax.plot(ages,values[i],label=label,c=color_sequence[i],linewidth=2)
+			if label == 'number of citations':
+				cits = values[i]
+				continue
 
+			ax1.plot(ages,values[i],label=label,c=color_sequence[i],linewidth=2)
 
-		ax.set_xlabel('citation delay')
-		ax.set_ylabel('percentage')
-		ax.legend()
+		ax1.set_xlabel('citation delay')
+		ax1.set_ylabel('percentage')
+		ax1.legend()
 
-		ax2 = axes[1]
+		ax = axes[0,0]
+		ax.plot(ages,cits)
+		ax1.set_xlabel('citation delay')
+		ax1.set_ylabel('percentage of citations')
+
+		ax2 = axes[1,0]
 
 		ax2.plot(ages,num_of_subjects,c=color_sequence[2])
 		ax2.set_xlabel('ciattion delay')
 		ax2.set_ylabel('number of subjects')
 		plt.tight_layout()
 
-		ax3 = axes[2]
-		ax2.plot(ages,ICRs,c=color_sequence[2])
-		ax2.set_xlabel('ciattion delay')
-		ax2.set_ylabel('ICRs')
+		ax3 = axes[1,1]
+		ax3.plot(ages,ICRs,c=color_sequence[2])
+		ax3.set_xlabel('ciattion delay')
+		ax3.set_ylabel('ICRs')
 		plt.tight_layout()
 
 		plt.savefig('pdf/high_cascade/{:}.jpg'.format(pid.replace(':','_')),dpi=400)
