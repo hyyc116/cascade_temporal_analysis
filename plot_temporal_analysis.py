f6af743b16baf09ab89cadc08888660de4cc89f3
黄永 <huangyong@140-182-74-92.ssl-vpn.indiana.edu>
2018-04-14 17:21:13 -0400

    add

--- plot_temporal_analysis.py
+++ plot_temporal_analysis.py
@@ -20,11 +20,14 @@
 
 from basic_config import *
 
-def plot_curve(highly_cited_paper_age_stat_path):
-
+def plot_curve_of_all_attrs(highly_cited_paper_age_stat_path):
+	logging.info('loading highly cited papers age statistics ...')
 	highly_cited_paper_age_stat = json.loads(open(highly_cited_paper_age_stat_path).read())
 
+	logging.info('plotting ...')	
 	for pid in highly_cited_paper_age_stat.keys():
+		logging.info('plot {:} ...'.format(pid))
+
 		age_stat = highly_cited_paper_age_stat[pid]
 
 		ages = []
@@ -36,10 +39,11 @@ def plot_curve(highly_cited_paper_age_stat_path):
 			values.append(attrs)
 
 
-		labels = ['numer of citations','late endorser','connector','normal endorser','depth','ICRs$','number of subjects','subjects']
+		labels = ['numer of citations','late endorser','connector','normal endorser','depth','ICRs','number of subjects','subjects']
 		values = zip(*values)
 		
-		plt.figure(figsize=(7,5))
+		fig,axes = plt.subplots(1,2,figsize=(14,5))
+		ax = axes[0]
 		for i,label in enumerate(labels):
 
 			if label == 'depth':
@@ -53,14 +57,28 @@ def plot_curve(highly_cited_paper_age_stat_path):
 				subjects = values[i]
 				continue
 
-			plt.plot(ages,values[i],label=label,c=color_sequence[i],linewidth=2)
+			ax.plot(ages,values[i],label=label,c=color_sequence[i],linewidth=2)
 
 
-		plt.xlabel('citation delay')
-		plt.ylabel('percentage')
+		ax.set_xlabel('citation delay')
+		ax.set_ylabel('percentage')
+
+		ax2 = axes[1]
+
+		ax2.plot(ages,num_of_subjects,c=color_sequence[2])
+		ax2.set_xlabel('ciattion delay')
+		ax2.set_ylabel('number of subjects')
 		plt.tight_layout()
 
-		plt.savefig('pdf/')
+		plt.savefig('pdf/high_cascade/{:}.jpg'.format(pid.replace(':','_')),dpi=400)
+		logging.info('saved to pdf/high_cascade/{:}.jpg ...'.format(pid.replace(':','_')))
+
+	
+	logging.info('Done')
+
+if __name__ == '__main__':
+	highly_cited_paper_age_stat_path = 'data/highly_cited_paper_age_stats.json'
+	plot_curve_of_all_attrs(highly_cited_paper_age_stat_path)
 
 
 
