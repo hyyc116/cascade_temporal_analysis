f3749b55bfe1a1439f52a54e93b46672392405de
hyyc116 <hyyc116@gmail.com>
2018-11-01 17:59:10 +0800

    modify sub-cascade

--- subcascade.py
+++ subcascade.py
@@ -247,7 +247,7 @@ def plot_num_of_comps(pathObj):
 
     ### 对不同的slice进行可视化
     NS=[10,20,50,100,200,500]
-    slice_datass = {}
+    slice_datas = {}
     for cnum in sorted(cnum_nums.keys()):
 
         cnum_xs.append(cnum)
