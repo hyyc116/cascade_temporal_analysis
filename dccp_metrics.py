f81144a25dcbea9906c63b6c2e5ce340bdef76e1
hyyc116 <hyyc116@gmail.com>
2019-11-03 20:28:51 +0800

    add

--- dccp_metrics.py
+++ dccp_metrics.py
@@ -366,7 +366,7 @@ def plot_dccps():
     for subj in sorted(subj_eins.keys()):
 
 
-        eins = [float('{:.2f}'.format(ein)) for ein in subj_eins[subj]]
+        eins = [f for f in [float('{:.2f}'.format(ein)) for ein in subj_eins[subj]] if f>0]
 
         logging.info('subj:{},len:{}'.format(subj,len(eins)))
 
