91967264146c1f4fccbbf7dff3ec940ff2aa615a
hyyc116 <hyyc116@gmail.com>
2020-04-21 13:00:57 +0800

    add

--- keyword_stat.py
+++ keyword_stat.py
@@ -61,8 +61,35 @@ def read_keywords_plus():
     logging.info('pid_keywords saved to data/wos_keywords_plus.txt')
 
 
+# 将所有关键词都合并起来，并将文章信息联合起来
+
+def read_year():
+
+    query_op = dbop()
+    sql = 'select id,sortdate from wos_core.wos_summary'
+    progress=0
+    pid_date = {}
+    saved_path = 'data/pid_keywords_plus.json'
+    os.remove(saved_path) if os.path.exists(saved_path) else None
+    for _id,date in query_op.query_database(sql):
+        progress+=1
+        pid_date[_id]= date
+
+        if progress%1000000==0:
+            logging.info('progress:{} ...'.format(progress))
+
+    open(saved_path,'w').write(json.dumps(pid_date))
+
+    logging.info('pid_date saved to {}'.format(saved_path))
+
+
+
+
+
+
 
 if __name__ == '__main__':
     # read_keywords()
-    read_keywords_plus()
+    # read_keywords_plus()
+    read_year()
 
