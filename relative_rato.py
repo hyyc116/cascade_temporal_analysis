e5314f419fb34fee9f887fdd32666c37b5afa960
hyyc116 <hyyc116@gmail.com>
2020-09-03 19:07:27 +0800

    Update relative_rato.py

--- relative_rato.py
+++ relative_rato.py
@@ -206,7 +206,7 @@ def plot_rr_figure():
 
         #     new_ys.append(np.mean(ys[start:end]))
 
-        xs,new_ys  = lowess(xs,ys)
+        xs,new_ys,_,_  = lowess(xs,ys)
         plt.plot(xs,new_ys,label=subj)
 
     plt.legend(prop={'size':6})
